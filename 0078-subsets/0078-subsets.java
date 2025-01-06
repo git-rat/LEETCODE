787c5e6d6ca5bcc05fc4fa9acf858ff9a7e501c9
yolorat <166698452+yolorat@users.noreply.github.com>
2025-01-06 21:35:12 +0530

    Time: 1 ms (54.94%), Space: 43 MB (25.56%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,23 @@
+
+
+class Solution {
+    void bt(List<List<Integer>> l , List<Integer> t , int[] nums , int s , boolean[] used){
+        l.add(new ArrayList<>(t));
+        for(int i =s  ; i< nums.length ; i++){
+        if(i>0 && nums[i]== nums[i-1] && !used[i-1]){
+            continue;
+        }
+        used[i]=true;
+        t.add(nums[i]);
+        bt(l , t , nums , i+1 , used);
+        used[i]=false;
+        t.remove(t.size()-1);
+    }}
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> l = new ArrayList<>();
+        Arrays.sort(nums);
+        bt(l , new ArrayList<>() ,nums ,0 , new boolean[nums.length] );
+        return l;
+    }
+}
+

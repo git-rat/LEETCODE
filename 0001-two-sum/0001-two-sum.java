65dbd49ad69e38a89ad612e259bbb90caf39b392
rat_for_git <166698452+git-rat@users.noreply.github.com>
2025-03-07 15:25:26 +0530

    Time: 131 ms (5.08%), Space: 44.9 MB (61.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,19 @@
+
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] ret ={0,0};
-         for (int i=0; i<nums.length;i++){
-        for (int j=i+1; j<nums.length ;j++){
-            if (  nums[i] + nums[j] == target ){
-               ret[0]=i;
-               ret [1] = j;
-               
-                 }
-          
+   
+    public static int[] twoSum(int[] nums, int target) {
+        int[] r = new int[2];
+       for(int i =0;i<nums.length;i++){
+        for(int j=0;j<nums.length;j++){
+            if(i==j) continue;
+            if(nums[i]+ nums[j] == target){
+                r = new int[]{i,j};
+            }
+
+        }
+       }return r;
     }
-}
-return ret;
-    }
+
+       
 }

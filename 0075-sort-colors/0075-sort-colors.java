9b1550989fd86f2724bfd9ddc7c7a07896eddefa
rat_for_git <166698452+git-rat@users.noreply.github.com>
2025-07-31 21:52:52 +0530

    Time: 1 ms (20.51%), Space: 42 MB (66.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,23 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-      int c0 =0, c1 =0;
-      for(int i : nums){
-        if(i == 0){
-            c0++;
-        }if(i ==1){
-            c1++;
+        HashMap<Integer,Integer> d = new  HashMap<>();
+        for(int i =0;i<nums.length;i++){
+                d.put(nums[i],d.getOrDefault(nums[i],0)+1);
         }
-      }
-      int k=0;
-      for(int i=0;i<c0;i++){
-        nums[k++] = 0;
-      }for(int i=0;i<c1;i++){
-        nums[k++] = 1;
-      }
-      for(int i=c1+c0; i < nums.length;i++){
-        nums[k++] = 2;
-      }
+        for(int i=0;i<=d.getOrDefault(0,0)-1;i++){
+            nums[i] = 0;
+        }
+        for(int i=d.getOrDefault(0,0);i<(d.getOrDefault(1,0)+d.getOrDefault(0,0));i++){
+            nums[i] = 1;
+        }
+        for(int i=(d.getOrDefault(1,0)+d.getOrDefault(0,0));i<nums.length;i++){
+            nums[i] = 2;
+        }
 
-    }    // getOrDefault use rather than get - or error for returning null
-    
-}     
+    }
+}

46cf7358f9f26dadac06653086a810daaf12b9c8
rat_for_git <166698452+git-rat@users.noreply.github.com>
2025-09-15 19:00:42 +0530

    Time: 2 ms (5.91%), Space: 42.4 MB (43.05%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0,j=0, k=0;
+        int[] arr = new int[m+n];
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+               arr[k++] =  nums1[i++];
+            }else{
+                arr[k++] =  nums2[j++];
+            }
+        }
+        while(i<m){
+            arr[k++] =  nums1[i++];
+        }
+         while(j<n){
+            arr[k++] =  nums2[j++];
+        }int p=0;
+        for(int x : arr){
+            nums1[p++] = x;
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+}

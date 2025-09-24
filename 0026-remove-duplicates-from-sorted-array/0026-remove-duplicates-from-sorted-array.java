232fe31c01e3aebae9d2efc484c2001594f6be46
rat_for_git <166698452+git-rat@users.noreply.github.com>
2025-09-24 11:57:30 +0530

    Time: 1 ms (59.21%), Space: 44.9 MB (62.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int i=0, j=0;
+       int i=0, j=1;
        while(j != nums.length){
         if(nums[i]==nums[j]){
             j++;
